e1fafbc93edbe4552e0f3eb7b9f4b678513d246b
jyh-centos9 <568000273@qq.com>
2025-03-28 22:09:50 +0800

    feat:add dialect and table creat-drop-exists

--- session/raw.go
+++ session/raw.go
@@ -4,17 +4,24 @@ import (
 	"database/sql"
 	"strings"
 
+	"github.com/KinoHui/project-jorm/dialect"
 	"github.com/KinoHui/project-jorm/log"
+	"github.com/KinoHui/project-jorm/schema"
 )
 
 type Session struct {
-	db      *sql.DB
-	sql     strings.Builder
-	sqlVars []interface{}
+	db       *sql.DB
+	dialect  dialect.Dialect
+	refTable *schema.Schema
+	sql      strings.Builder
+	sqlVars  []interface{}
 }
 
-func New(db *sql.DB) *Session {
-	return &Session{db: db}
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
 }
 
 func (s *Session) Clear() {
